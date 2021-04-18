a895eec50eb15b412040150c06ea527d44cc6b72
Fernando <f.sanchezvilela@mrmilu.com>
2021-04-18 19:17:10 +0200

    Imports, Exports, and Webpack

--- src/app/alert.service.js
+++ src/app/alert.service.js
@@ -1,21 +1,22 @@
-class AlertService{
-  constructor(){
-    this.errorBox = document.getElementById("error");
+import { inputsAreValid } from './utils/inputs-are-valid';
+export class AlertService {
+  constructor() {
+    this.errorBox = document.getElementById('error');
   }
 
-  handleAdditionError(inputs, numbers){
-    const fullMessage = inputs.reduce((message,str,index) => {
-      if(inputsAreValid(numbers[index])){
-        return message + "";
+  handleAdditionError(inputs, numbers) {
+    const fullMessage = inputs.reduce((message, str, index) => {
+      if (inputsAreValid(numbers[index])) {
+        return message + '';
       } else {
         return message + `${str} is not a number. `;
       }
-    }, "Please enter two valid numbers!");
-    this.errorBox.classList.remove("invisible");
+    }, 'Please enter two valid numbers!');
+    this.errorBox.classList.remove('invisible');
     this.errorBox.innerText = fullMessage;
   }
 
-  hideErrors(){
-    this.errorBox.classList.add("invisible")
+  hideErrors() {
+    this.errorBox.classList.add('invisible');
   }
 }
