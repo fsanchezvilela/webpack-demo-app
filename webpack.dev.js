57f9d5c3ccc5ed62eef8adc449ebadab9ea45256
Fernando <f.sanchezvilela@mrmilu.com>
2021-04-19 00:14:34 +0200

    Extract and Minify CSS

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -9,10 +10,27 @@ module.exports = merge(common, {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', // 2. Turns css into commonjs
+          'sass-loader', // 1. Turns sass into css
+        ],
+      },
+    ],
+  },
   //conflict with port 8080;
   devServer: {
     historyApiFallback: true,
     contentBase: './',
-    port: 3000 // <--- Add this line and choose your own port number
-  }
+    port: 3000, // <--- Add this line and choose your own port number
+  },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,7 +13,37 @@ module.exports = merge(common, {
     //cache busting and plugins
     filename: '[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]'
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
-  plugins:[new CleanWebpackPlugin()]
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerWebpackPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/template.html',
+        minify:{
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        }
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract css into files
+          'css-loader', // 2. Turns css into commonjs
+          'sass-loader', // 1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
