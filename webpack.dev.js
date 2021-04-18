fea2b59fb5406c2246d5b285d003fee031df6006
Fernando <f.sanchezvilela@mrmilu.com>
2021-04-18 21:23:57 +0200

    Splitting Dev & Production

---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    //cache busting and plugins
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  //conflict with port 8080;
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 3000 // <--- Add this line and choose your own port number
+  }
+});
